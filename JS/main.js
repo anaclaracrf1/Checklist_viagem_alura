42de480c83f3342857cce9b6496e2ea0c0d77619
anas2 <ana05clara24@gmail.com>
2023-06-16 18:15:55 -0300

    Pegando os dados do input e começando a criar a function

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,12 @@
 const novo_item = document.getElementById('novoItem')
 
-novo_item.addEventListener("submit", (evento) =>) {
+novo_item.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    console.log('worked')
+    
+    Cria_elemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function Cria_elemento(nome,quantidade){
+    console.log(nome, quantidade)
+
 }
