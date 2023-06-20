11669970a7a3aa18ed5ea9d67e31211137dbf69d
anas2 <ana05clara24@gmail.com>
2023-06-19 21:07:10 -0300

    adicionando item e começando o processo de salvar no local storage

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,32 @@
 const novo_item = document.getElementById('novoItem')
+const lista = document.getElementById('lista')
 
 novo_item.addEventListener("submit", (evento) => {
     evento.preventDefault()
+
+    const nome = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
     
-    Cria_elemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    Cria_elemento(nome, quantidade)
+
+    nome = ""
+    quantidade = ""
 })
 
-function Cria_elemento(nome,quantidade){
-    console.log(nome, quantidade)
 
+function Cria_elemento(nome,quantidade){
+
+    const NovoItem = document.createElement('li')
+    NovoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    NovoItem.appendChild(numeroItem)
+    NovoItem.innerHTML += nome
+
+    lista.appendChild(NovoItem)
+
+    localStorage.setItem("nome", nome)
+    localStorage.setItem("quantidade", quantidade)
 }
